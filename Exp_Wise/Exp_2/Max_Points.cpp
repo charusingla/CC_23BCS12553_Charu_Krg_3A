b8d60a447d24680b98c8c4d2ab4d24090c44387d
charusingla <charusingla38@mail.com>
2026-04-03 14:17:35 +0530

    Done

---
+++ Exp_Wise/Exp_2/Max_Points.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int maxPoints(vector<vector<int>>& points) {
+        int n = points.size();
+        int ans = 1;
+        
+        for (int i = 0; i < n - 1; i++) {
+            unordered_map<double, int> mp;
+            for (int j = i + 1; j < n; j++) {
+                double slope = (double)(points[j][1] - points[i][1]) /
+                               (points[j][0] - points[i][0]);
+                if ((points[j][1] - points[i][1]) < 0 &&
+                    (points[j][0] - points[i][0]) == 0) {
+                    mp[abs(slope)]++;
+                } else
+                    mp[slope]++;
+            }
+            int res = 0;
+            for (auto& [slp, cnt] : mp) {
+                res = max(res, cnt + 1);
+            }
+            ans = max(ans, res);
+        }
+
+        return ans;
+    }
+};
